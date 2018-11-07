82021b35993f78fa768217b91c98840f26f36097
ColaCW <2629080132@qq.com>
2018-11-07 09:41:14 +0800

    admin

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="msg">{{mainMsg}}</div>
+    <button @click="sys()">点</button>
+  </div>
+  <!--<div class="login_div">-->
+<!--1111-->
+  <!--</div>-->
+</template>
+
+<style scoped>
+  @import "../assets/css/login.css";
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        mainMsg: '我是主页组件',
+        index:1
+      }
+    },
+    mounted:function(){
+      this.init();
+    },
+    methods: {
+      init:function(){
+        $("canvas").css("position","absolute");
+        $("canvas").css("top","0");
+        $("canvas").css("left","0");
+      },
+      sys:function () {
+        var that = this;
+        console.log($(".msg").html()+that.index);
+        that.index++;
+      }
+    }
+  }
+
+</script>
