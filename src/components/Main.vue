e9a6042859c75313bcdb97c0ae72875ba67e02a1
ColaCW <2629080132@qq.com>
2019-04-16 15:46:35 +0800

    上传及首页的一点小修改

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    首页
+  <div style="font-size: 20px;text-align: center;width: 10%;margin: 20% auto">
+    <div style="width: 30px;float: left;">逆水行舟</div>
+    <div style="width: 30px;float: right">不进则退</div>
+    <div style="clear: both"></div>
   </div>
 </template>
 <script>
